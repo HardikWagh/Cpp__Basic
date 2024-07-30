4767ef04458f4480ffc148ae658bcac57ea8ee7c
HardikWagh <161699531+HardikWagh@users.noreply.github.com>
2024-07-30 10:51:57 +0530

    Create arithmetic.cpp
    Created: arithmetic.cpp

---
+++ arithmetic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    int a;
+    int b;
+    std::cin>>a;
+    std::cin>>b;
+    std::cout<<"Addition is: "<<a+b<<"\n";
+    std::cout<<"Substraction is: "<<a-b<<"\n";
+    std::cout<<"Multiplication is: "<<a*b<<"\n";
+    std::cout<<"Division is: "<<a/b<<"\n";
+    return 0;
+}
